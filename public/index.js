1e69143442faf1b4e63aa2fd3ee4c9569e90b1b7
Valkorz <pivarcivittorio@gmail.com>
2024-09-20 14:04:49 -0300

    Added dynamic loading from json and updated style

--- public/index.js
+++ public/index.js
@@ -1,12 +1,43 @@
 
 //handle custom properties of the item card class
 document.addEventListener('DOMContentLoaded', function() {
-    const itemCards = document.querySelectorAll('.itemCard');
-    itemCards.forEach(card => {
-        const imgSrc = card.getAttribute('img-src');
-        const imgElement = card.querySelector('img');
-        if(imgElement && imgSrc){
-            imgElement.src = imgSrc;
-        }
-    })
+
+    fetch('data/pizzas.json')
+        .then(response => response.json())
+        .then(pizzaFlavors => {
+            const pizzaContainer = document.getElementById('pizzaContainer');
+
+            pizzaFlavors.forEach(pizza => {
+                const itemCard = document.createElement('div');
+                itemCard.className = 'itemCard';
+                itemCard.setAttribute('img-src', pizza.imgSrc);
+                itemCard.innerHTML = `
+                    <h3 id="name">${pizza.name}</h3>
+                    <img alt=${pizza.name} src=${pizza.imgSrc}>
+                    <div class="itemCardFooter">
+                        <h5 id="description">${pizza.description}<h5>
+                        <h3 id="price">${pizza.price}</h3>
+                    </div>
+                    <button></button>
+                `;
+                pizzaContainer.appendChild(itemCard);
+
+                //Add event listener to each card to allow for functionality;
+
+                const button = itemCard.querySelector('button');
+                button.addEventListener('click', () => {
+                    alert(`Button clicked for ${pizza.name}`);
+                })
+            });
+        })
+        .catch(error => console.error('Error fetching the JSON data:', error));
+
+    // const itemCards = document.querySelectorAll('.itemCard');
+    // itemCards.forEach(card => {
+    //     const imgSrc = card.getAttribute('img-src');
+    //     const imgElement = card.querySelector('img');
+    //     if(imgElement && imgSrc){
+    //         imgElement.src = imgSrc;
+    //     }
+    // })
 })
